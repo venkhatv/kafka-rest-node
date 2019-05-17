5c879c5aeb1ea64dca8591556c314af732098bbb
Venkhat Vairavan <venkhat.vairavan@nutanix.com>
2019-05-17 15:03:06 +0530

    Adds support for other v2 consumer methods and supports Promise

--- examples/console_consumer.v2.js
+++ examples/console_consumer.v2.js
@@ -63,47 +63,54 @@ kafka.consumer(consumerGroup).join(consumerConfig, async function(err, consumer_
     console.log("Consumer instance initialized: " + consumer_instance.toString());
     var stream;
     try {
-    stream = await consumer_instance.subscription({ topics: [topicName]}, {requestDelay: 1000*10})
+        stream = await consumer_instance.subscription({ topics: [topicName]}, {requestDelay: 1000*10})
 
-    stream.on('data', function(msgs) {
-        for(var i = 0; i < msgs.length; i++) {
-            if (format == "binary") {
-                // Messages keys (if available) and values are decoded from base64 into Buffers. You'll need to decode based
-                // on whatever serialization format you used. By default here, we just try to decode to text.
-                console.log(msgs[i].value.toString('utf8'));
-                // Also available: msgs[i].key, msgs[i].partition
-            } else {
-                console.log(JSON.stringify(msgs[i].value));
+        stream.on('data', function(msgs) {
+            for(var i = 0; i < msgs.length; i++) {
+                if (format == "binary") {
+                    // Messages keys (if available) and values are decoded from base64 into Buffers. You'll need to decode based
+                    // on whatever serialization format you used. By default here, we just try to decode to text.
+                    console.log(msgs[i].value.toString('utf8'));
+                    // Also available: msgs[i].key, msgs[i].partition
+                } else {
+                    console.log(JSON.stringify(msgs[i].value));
+                }
             }
-        }
 
-        consumed += msgs.length;
-        if (messageLimit !== undefined && consumed >= messageLimit)
+            consumed += msgs.length;
+            if (messageLimit !== undefined && consumed >= messageLimit)
+                consumer_instance.shutdown(logShutdown);
+        });
+        stream.on('error', function(err) {
+            console.log("Consumer instance reported an error: " + err);
+            console.log("Attempting to shut down consumer instance...");
             consumer_instance.shutdown(logShutdown);
-    });
-    stream.on('error', function(err) {
-        console.log("Consumer instance reported an error: " + err);
-        console.log("Attempting to shut down consumer instance...");
-        consumer_instance.shutdown(logShutdown);
-    });
-    stream.on('end', function() {
-        console.log("Consumer stream closed.");
-    });
-    var topicsObj = await consumer_instance.getSubscriptionDetails();
-    console.log("Subscribed Topics: " + topicsObj.topics.join());
-    
-    // Events are also emitted by the parent consumer_instance, so you can either consume individual streams separately
-    // or multiple streams with one callback. Here we'll just demonstrate the 'end' event.
-    consumer_instance.on('end', function() {
-        console.log("Consumer instance closed.");
-    });
+        });
+        stream.on('end', function() {
+            console.log("Consumer stream closed.");
+        });
+        var topicsObj = await consumer_instance.getSubscription( (err,result) => {
+            console.log("Subscribed Topics: " + result.topics.join());
+        });
+        console.log("Subscribed Topics: " + topicsObj.topics.join());
+        
+        // setTimeout(() => {
+        //     var topicsObj = await consumer_instance.cancelSubscription();
+        //     console.log("Subscribed Topics: " + topicsObj.topics.join());
+        // }, 30000)
+        
+        // Events are also emitted by the parent consumer_instance, so you can either consume individual streams separately
+        // or multiple streams with one callback. Here we'll just demonstrate the 'end' event.
+        consumer_instance.on('end', function() {
+            console.log("Consumer instance closed.");
+        });
 
-    // Also trigger clean shutdown on Ctrl-C
-    process.on('SIGINT', function() {
-        console.log("Attempting to shut down consumer instance...");
-        consumer_instance.shutdown(logShutdown);
-    });
-    }catch(err){
+        // Also trigger clean shutdown on Ctrl-C
+        process.on('SIGINT', function() {
+            console.log("Attempting to shut down consumer instance...");
+            consumer_instance.shutdown(logShutdown);
+        });
+    } catch(err) {
         console.log('Failed '+err);
     }
 
